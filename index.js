fa4a4db76d2e0588423c77bf7d7ca328e82b4a6f
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2021-02-13 23:11:25 +0000

    add nodemon as dev dep

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/a', (req, res) => {
+  res.send('Hello!')
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

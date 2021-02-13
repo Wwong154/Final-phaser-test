ec8ce3a5c835a9a4588d8b6c36b7476ace8a0fe5
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2021-02-13 23:29:04 +0000

    add morgan, dotenv, phaser to the init server

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.ENV || "development";
+const express = require('express')
+const app = express()
+const morgan = require('morgan');
+
+//init of program use
+app.use(morgan('dev'));
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get('/a', (req, res) => {
+  res.send('Hello!')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})

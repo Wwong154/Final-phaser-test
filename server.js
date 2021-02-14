f2d9df142944085e6e73f18d2ea916cb828b6e26
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2021-02-14 01:06:12 +0000

    init complete

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const ENV = process.env.ENV || "development";
 const express = require('express')
 const app = express()
 const morgan = require('morgan');
+const path = require('path');
 
 //init of program use
 app.use(morgan('dev'));
@@ -16,6 +17,10 @@ app.use(express.static("public"));
 app.get('/a', (req, res) => {
   res.send('Hello!')
 })
+
+app.get("/game", (req, res) => {
+  res.sendFile(path.join(__dirname, "js", "game.js"), err => console.log(err));
+});
 
 app.get('/', (req, res) => {
   res.render('index')
